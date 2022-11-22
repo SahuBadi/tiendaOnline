7a30f6b2228ac7a49d55f9028a55390413b0e254
SahuBadi <mdsahuquillo@gmail.com>
2022-11-22 20:21:17 +0100

    módulo funcionalidad boton plus-minus

--- js/plus-minus-button.js
+++ js/plus-minus-button.js
@@ -1,18 +1,34 @@
 console.log("hola")
-
 export let renderPlusMinusButton = () => {
-    let cantidad = document.getElementById("cantidad");
-    let plus = document.getElementById("plus");
-    let minus = document.getElementById("minus");
 
-    
-    plus.addEventListener("click", () => {
-        cantidad.value = (parseInt(cantidad.value) + 1);
+    let plus = document.querySelectorAll(".plus-button > button");
+    let minus = document.querySelectorAll(".minus-button > button");
+
+    plus.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            let cantidad = button.closest('.plus-minus-button').querySelector('.amount input');
+
+            console.log(cantidad)
+
+            if (cantidad.value < 50){ 
+                cantidad.value = (parseInt(cantidad.value) + 1)
+            }
+        })
     })
 
-    minus.addEventListener("click", () => {
-        if (cantidad.value > 1){
-            cantidad.value = (parseInt(cantidad.value) - 1);
-        }
+    minus.forEach(button => {
+
+        button.addEventListener("click", () => {
+
+            let cantidad = button.closest('.plus-minus-button').querySelector('.amount input');
+
+            if (cantidad.value > 1){
+                cantidad.value = (parseInt(cantidad.value) - 1);
+            }
+        })
     })
+
+
 }
