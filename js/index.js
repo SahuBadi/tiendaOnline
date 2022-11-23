3af117a37cb881c36ab83afe622fdcb00e1e37b5
SahuBadi <mdsahuquillo@gmail.com>
2022-11-23 13:46:34 +0100

    Module para etiquetas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
 import { renderPlusMinusButton } from "./plus-minus-button.js";
+import { renderTextTabs} from "./tabs-selector.js"
 
 renderPlusMinusButton();
+renderTextTabs();
---
+++ js/tabs-selector.js
@@ -0,0 +1,38 @@
+export let renderTextTabs = () => {
+
+    let tabelements = document.querySelectorAll(".tab");
+    let tabcontents = document.querySelectorAll(".content-tab");
+
+
+    tabelements.forEach(tab => {
+
+        tab.addEventListener("click", () => { 
+
+            tabelements.forEach(tab => {
+                tab.classList.remove("active");
+            })
+
+            tab.classList.add("active");
+
+            tabcontents.forEach(content => {
+
+                if(tab.dataset.index == content.dataset.index) {
+                    content.classList.add("active")
+                }else{
+                    content.classList.remove("active")    
+                }
+            })
+
+        })
+    })
+}
+
+
+
+/*
+            if (tab.classList.contains("active")){
+                tab.classList.remove("active");
+            } else {
+                tab.classList.add("active");
+            }
+*/
